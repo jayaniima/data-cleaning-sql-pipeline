34c0afaf92a6e7f3a663f2c8c2eb2df801e95ac8
Jayani Imalka <149018228+jayaniima@users.noreply.github.com>
2026-06-16 00:31:41 -0400

    Create contact_fixes.sql

---
+++ rules/contact_fixes.sql
@@ -0,0 +1,11 @@
+--Formatting for Country code
+
+UPDATE dbo.VMP_Contacts_Stage
+SET PhoneNum =  '+1 ' + PhoneNum,
+     FaxNum  =  '+1 ' + FaxNum
+WHERE VMVN# IN (SELECT VMVN# FFROM Company..VMP WHERE Country = 'US'); -- +1 123 123 1234
+
+UPDATE dbo.VMP_Contacts_Stage
+SET PhoneNum =  '+1' + REPLACE(PhoneNum,' ','')
+     FaxNum  =  '+1' + REPLACE(FaxNum,' ','')
+WHERE VMVN# IN (SELECT VMVN# FFROM Company..VMP WHERE Country = 'CA'); --+11231231234
